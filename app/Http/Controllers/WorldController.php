8f01c590d2ed8eb5e96805703dbc418cd6002ad4
indumathi01 <43129651+indumathi01@users.noreply.github.com>
2018-12-31 18:46:49 +0530

    Update WorldController.php

--- app/Http/Controllers/WorldController.php
+++ app/Http/Controllers/WorldController.php
@@ -12,7 +12,7 @@ class WorldController extends Controller
         // ?comment=foobar
         $comment = $request->input('comment');
 
-        $msg = 'Welcome: Have a nice day From Zippyops';
+        $msg = 'Welcome All: Have a nice day From Zippyops';
 
         if ($message != null) {
             $msg = $message;
